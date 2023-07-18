5940b126efc63bc77e3fcfbef26d35a2630b70b7
Swaminathan S K <swamisathya2004@gmail.com>
2023-07-19 02:37:38 +0530

    RRT_connect

--- Scripts/dump1.py
+++ Scripts/dump1.py
@@ -1,7 +1,10 @@
 import RRT_star as RRT_star
 import RRT as RRT
+import RRT_star2
+import RRT_connect
 import cv2
 
-model = RRT_star.RRT_star((473, 365), (100, 100), 20000, 50, 30)
+# model = RRT_connect.RRT_star((473, 365), (452, 213), 300, 50, 30)
+model = RRT_connect.RRT_star((347, 195), (452, 213), 3000, 100, 30)
 model.read_image("processed_map.png")
 model.run_loop()
